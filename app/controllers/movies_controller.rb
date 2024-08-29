10abefa98e155354d13a17708c2c81ecb4266dbc
mandaluzr <mandaluzr@hotmail.com>
2024-08-29 10:08:43 +0200

    add update route, method and make it work properly

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,4 +11,11 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def update
+    @movie = Movie.find(params[:id])
+    movie_params = params.require(:movie).permit(:title, :rating, :total_gross, :description, :released_on)
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
 end
