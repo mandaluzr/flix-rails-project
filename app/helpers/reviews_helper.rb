3ed937df0b4e8a41d0af6e55a31b35beb3be8676
mandaluzr <mandaluzr@hotmail.com>
2024-11-27 10:25:47 +0100

    show reviews in each movie

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,5 @@
 module ReviewsHelper
+    def time_ago_in_words_for_review(review)
+        "#{time_ago_in_words(review.created_at)} ago"
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  resources :reviews
     # Ruta raíz de la aplicación, apunta a MoviesController#index
     root "movies#index"
   
     # Rutas RESTful para el controlador Movies
-    resources :movies
+    resources :movies do
+      resources :reviews
+    end
   
     # Rutas para el controlador Tmdb, con solo la acción de búsqueda
     resources :tmdb, only: [] do
@@ -13,5 +14,6 @@ Rails.application.routes.draw do
         get :search  # Esto generará la ruta /tmdb/search para realizar búsquedas
       end
     end
+    
   end
   
