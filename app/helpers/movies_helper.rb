6b345391ec66570acdb7a01d10e1a6688959f446
mandaluzr <mandaluzr@hotmail.com>
2024-12-10 07:39:59 +0100

    add average stars

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -12,4 +12,12 @@ module MoviesHelper
     movie.released_on.strftime('%B %d at %I:%M %P')
  end
 
+  def average_stars(movie)
+    if movie.average_stars.zero?
+      content_tag(:strong, 'No reviews')
+    else
+      pluralize(number_with_precision(movie.average_stars, precision: 1), 'star')
+    end
+  end
+
 end
